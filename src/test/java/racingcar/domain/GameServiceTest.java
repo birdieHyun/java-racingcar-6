0864c95b3e920c6a44c68d8b75a677255f3519e1
birdieHyun <flaehdan@gmail.com>
2023-10-29 20:21:55 +0900

    test: 우승자를 가려내는 테스트 코드 작성 (#12)

---
+++ src/test/java/racingcar/domain/GameServiceTest.java
@@ -0,0 +1,63 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+
+class GameServiceTest {
+
+    private GameService gameService;
+
+    @BeforeEach
+    void setUp() {
+
+        gameService = new GameService();
+    }
+
+    @Test
+    @DisplayName("우승자가 \"pobi\"인지 테스트")
+    void findWinnerTest() {
+        // given
+        Car pobi = Car.makeCar("pobi");
+        Car birdie = Car.makeCar("birdie");
+        pobi.move();
+
+        List<Car> cars = List.of(pobi, birdie);
+
+        // when
+        List<String> winners = gameService.findWinner(cars);
+
+        // then
+        assertAll(
+                () -> assertThat(winners).hasSize(1),
+                () -> assertThat(winners).containsExactly("pobi")
+        );
+    }
+
+    @Test
+    @DisplayName("우승자가 두명일 경우 두명인지 테스트")
+    void findTwoWinnerTest() {
+        // given
+        Car pobi = Car.makeCar("pobi");
+        Car birdie = Car.makeCar("birdie");
+        pobi.move();
+        birdie.move();
+
+        List<Car> cars = List.of(pobi, birdie);
+
+        // when
+        List<String> winners = gameService.findWinner(cars);
+
+        // then
+        assertAll(
+                () -> assertThat(winners).hasSize(2),
+                () -> assertThat(winners).containsExactly("pobi", "birdie")
+        );
+    }
+}
