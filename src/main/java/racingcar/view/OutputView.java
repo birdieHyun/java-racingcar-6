5ae85958758d913dd5c65eef37f784ae0e6f7238
birdieHyun <flaehdan@gmail.com>
2023-10-29 17:59:36 +0900

    feat: 시도할 횟수 묻는 메시지 출력 (#3)

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,9 +3,15 @@ package racingcar.view;
 public class OutputView {
 
     private static final String START_GAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_HOW_MANY_TIMES_TO_MOVE = "시도할 회수는 몇회인가요?";
 
     public void printStartGameMessage() {
 
         System.out.println(START_GAME_MESSAGE);
+    }
+
+    public void askHowManyTimesToMove() {
+
+        System.out.println(ASK_HOW_MANY_TIMES_TO_MOVE);
     }
 }
