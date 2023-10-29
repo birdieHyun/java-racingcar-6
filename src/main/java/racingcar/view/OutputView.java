eca7c96da02d33743aaa227bbce88a35a5af5fc0
birdieHyun <flaehdan@gmail.com>
2023-10-29 18:03:11 +0900

    feat: 게임 결과를 출력할 수 있다. (#8)

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,13 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 public class OutputView {
 
     private static final String START_GAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_HOW_MANY_TIMES_TO_MOVE = "시도할 회수는 몇회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String SEPARATOR =  " : ";
 
     public void printStartGameMessage() {
 
@@ -13,5 +17,15 @@ public class OutputView {
     public void askHowManyTimesToMove() {
 
         System.out.println(ASK_HOW_MANY_TIMES_TO_MOVE);
+    }
+
+    public void printResultMessage() {
+
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printResult(Car car) {
+
+        System.out.println(car.getName() + SEPARATOR + car.getPosition());
     }
 }
