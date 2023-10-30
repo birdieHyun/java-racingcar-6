e266c55f2820a399ec5cc99b60547f9a28c940ab
birdieHyun <flaehdan@gmail.com>
2023-10-30 21:13:38 +0900

    test: TryCount 생성 테스트 (#28)

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -2,13 +2,14 @@ package racingcar.domain;
 
 public record TryCount(int value) {
 
+    private static final String ERROR_MESSAGE = "[ERROR] 시도 횟수는 0보다 작거나, 1000보다 클 수 없습니다.";
     private static final int MIN_TRY_COUNT = 0;
     private static final int MAX_TRY_COUNT = 1000;
 
     public TryCount {
 
         if (value < MIN_TRY_COUNT || value > MAX_TRY_COUNT) {
-            throw new IllegalArgumentException("시도 횟수는 0보다 작을 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
 }
